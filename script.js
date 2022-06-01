3d6ab08bfbfb7857b6006fe5d9fbebb9b9762f6b
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-06-01 23:36:52 +0200

    Update

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.eu/rest/v2/name/portugal');
+request.send();
+
+request.addEventListener('load', function () {
+  console.log(this.responseText);
+
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+});
